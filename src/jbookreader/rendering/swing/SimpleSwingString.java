33483b963267f82fec5b8430962e3ab429f42b0c
Dmitry Baryshkov <dbaryshkov@gmail.com>
2007-01-03 04:54:33 +0300

    Fix incorrect placement of strings starting with \u2013

--- src/jbookreader/rendering/swing/SimpleSwingString.java
+++ src/jbookreader/rendering/swing/SimpleSwingString.java
@@ -1,6 +1,7 @@
 package jbookreader.rendering.swing;
 
 import java.awt.Graphics2D;
+import java.awt.font.LineMetrics;
 import java.awt.geom.Rectangle2D;
 
 import jbookreader.rendering.IDrawable;
@@ -20,10 +21,16 @@ final class SimpleSwingString implements IDrawable {
 		this.string = string;
 		this.font = font;
 
+		LineMetrics metrics = font.getFont().getLineMetrics(string, driver.getFontRC());
 		Rectangle2D bounds = font.getFont().getStringBounds(string, driver.getFontRC());
 //		System.out.println(bounds);
-		height = JGraphicDriver.pixelToDimension((float) bounds.getHeight());
-		depth = JGraphicDriver.pixelToDimension((float) (bounds.getHeight() + bounds.getY()));
+		height = JGraphicDriver.pixelToDimension(metrics.getAscent());
+		depth = JGraphicDriver.pixelToDimension(metrics.getDescent());
+		// XXX: There are some problems if we use bounds for height/depth:
+		//     if the first char is some type of long dash \u2013, then we get incorrect
+		//     measurements (the base line is shifted)
+//		height = JGraphicDriver.pixelToDimension((float) bounds.getHeight());
+//		depth = JGraphicDriver.pixelToDimension((float) (bounds.getHeight() + bounds.getY()));
 		width = JGraphicDriver.pixelToDimension((float) bounds.getWidth());
 	}
 
@@ -32,6 +39,7 @@ final class SimpleSwingString implements IDrawable {
 		if (!graphics.getFont().equals(font.getFont())) {
 			graphics.setFont(font.getFont());
 		}
+//		System.out.println(width + " x " + height + " + " + depth);
 		graphics.drawString(string,
 				(int) JGraphicDriver.dimensionToPixel(this.driver.horizontalPosition),
 				(int) JGraphicDriver.dimensionToPixel(this.driver.verticalPosition + height));
